597355f007437713819b9165dfcdbc0570ed5f19
lilywhale <94987363+lilywhale@users.noreply.github.com>
2024-02-08 22:57:39 +0100

    Update test.py

--- backEnd/test.py
+++ backEnd/test.py
@@ -16,53 +16,50 @@ class TestPreprocessText(unittest.TestCase):
         processed_text = preprocess_text(text)
         self.assertEqual(processed_text, "hello world")
 
-    # Add more tests for other aspects of preprocessing like stopword removal and stemming
-
 
 class TestSentimentPrediction(unittest.TestCase):
     def test_sentiment_prediction_positive(self):
-        # Mock input text and expected sentiment
         input_text = "I love this product"
-        expected_sentiment = "positive"
+        expected_sentiment = "Positive"
 
-        # Preprocess the input text
         processed_text = preprocess_text(input_text)
 
-        # Perform sentiment prediction
         text_vector = tfidf_vectorizer.transform([processed_text])
         predicted_sentiment = model.predict(text_vector)[0]
 
-        # Assert that the predicted sentiment matches the expected sentiment
         self.assertEqual(predicted_sentiment, expected_sentiment)
 
   def test_sentiment_prediction_negative(self):
-          # Mock input text and expected sentiment
           input_text = "I hate it this is so bad"
-          expected_sentiment = "negative"
+          expected_sentiment = "Negative"
   
-          # Preprocess the input text
           processed_text = preprocess_text(input_text)
   
-          # Perform sentiment prediction
           text_vector = tfidf_vectorizer.transform([processed_text])
           predicted_sentiment = model.predict(text_vector)[0]
   
-          # Assert that the predicted sentiment matches the expected sentiment
           self.assertEqual(predicted_sentiment, expected_sentiment)
 
   def test_sentiment_prediction_neutral(self):
-          # Mock input text and expected sentiment
-          input_text = "i am playing fifa right now"
-          expected_sentiment = "neutral"
-  
-          # Preprocess the input text
+          input_text = "I hate that this easy mayhem modifier event on mayhem won’t last forever."
+          expected_sentiment = "Neutral"
+  #Another successful stream last night
           processed_text = preprocess_text(input_text)
   
-          # Perform sentiment prediction
           text_vector = tfidf_vectorizer.transform([processed_text])
           predicted_sentiment = model.predict(text_vector)[0]
   
-          # Assert that the predicted sentiment matches the expected sentiment
+          self.assertEqual(predicted_sentiment, expected_sentiment)
+
+    def test_sentiment_prediction_irrelevant(self):
+          input_text = "Another successful stream last night"
+          expected_sentiment = "Irrelevant"
+
+        processed_text = preprocess_text(input_text)
+  
+          text_vector = tfidf_vectorizer.transform([processed_text])
+          predicted_sentiment = model.predict(text_vector)[0]
+  
           self.assertEqual(predicted_sentiment, expected_sentiment)
 
 
